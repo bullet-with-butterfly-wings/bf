a715162004dcb20088fefaa9a7e88f9ae6acac5c
bullet-with-butterfly-wings <dej.jonas12@gmail.com>
2024-07-21 15:58:05 +0200

    Quick fix

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -6,16 +6,18 @@ import SettingsIcon from '@mui/icons-material/Settings';
 const Sidebar = () => {
   const hello = () => {console.log("Settings")};
   return (
-    <div className="fixed top-0 left-0 h-full bg-red-600 text-white w-96 flex flex-col align-top content-between">
-      <ProfileCard name={"Jonas Dej"} university={"University of Cambridge"} imageUrl={"https://via.placeholder.com/150"} />      
-      <Tracker />
-      <Tracker />
-      <Tracker />
-      <div className='fixed bottom-10 w-96'>
-      <div className="flex align-middle justify-center">
-        <button className="w-2/3 rounded-full font-bold shadow" onClick={hello}><SettingsIcon /><span className='ml-2'>Settings</span></button>
+    <div className="fixed top-0 left-0 h-full bg-red-600 text-white w-96">
+    <div className=' flex flex-col h-full justify-between'>
+      <div className='flex align-top flex-col'>
+        <ProfileCard name={"Jonas Dej"} university={"University of Cambridge"} imageUrl={"https://via.placeholder.com/150"} />      
+        <Tracker />
+        <Tracker />
+        <Tracker />
       </div>
+      <div className="flex align-middle justify-center my-10">
+        <button className="w-2/3 rounded-full shadow font-bold text-xl" onClick={hello}><SettingsIcon /><span className='ml-2'>Settings</span></button>
       </div>
+    </div>
     </div>
   );
 };
