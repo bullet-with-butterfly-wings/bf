4b187921e09609b555fa3addea6fe023b5e00b5b
bullet-with-butterfly-wings <dej.jonas12@gmail.com>
2024-07-24 23:58:22 +0200

    Frontend finished ig

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -10,7 +10,7 @@ const Sidebar = () => {
     <div className="fixed top-0 left-0 h-full bg-red-600 text-white w-96">
     <div className=' flex flex-col h-full justify-between'>
       <div className='flex align-top flex-col'>
-        <ProfileCard name={"Jonas Dej"} university={"University of Cambridge"} imageUrl={"https://via.placeholder.com/150"} />      
+        <a href='/base' style={{all:"unset"}}><ProfileCard name={"Jonas Dej"} university={"University of Cambridge"} imageUrl={"https://via.placeholder.com/150"} /> </a>    
         <Tracker category = {"Tuition"} init={1000} current={400} money={"GBP"}/>
         <Tracker category = {"Accomodation"} init={1000} current={500} money={"GBP"}/>
         <Tracker category = {"Living Expenses"} init={1000} current={500} money={"GBP"}/>
